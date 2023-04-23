3ce29fe4356529eaf4358cdca0f6b660bf1fc7fc
liaoyi <cooltut@hotmail.com>
2023-04-23 18:03:53 +0800

    修复重命名错误

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/apps/accounts/admin.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/apps/accounts/admin.py
@@ -6,6 +6,32 @@ User = get_user_model()
 
 @admin.register(User)
 class UserAdmin(auth_admin.UserAdmin):
+    fieldsets = [
+        (
+            "个人信息",
+            {
+                "fields": (
+                    "username",
+                    "email",
+                    "password",
+                    "last_login",
+                    "date_joined",
+                )
+            },
+        ),
+        (
+            "权限",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+    ]
     list_display = [
         "username",
         "email",
